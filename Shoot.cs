d268ce44cabea1fcf7947b6d7e1903d2f5f18ef3
David Floyd <63920729+DavyjonesCode@users.noreply.github.com>
2024-12-07 12:05:26 -0600

    commented Shoot.cs

--- Shoot.cs
+++ Shoot.cs
@@ -1,3 +1,7 @@
+//David Floyd
+//help from this yt tutorial - https://www.youtube.com/watch?v=cjNMQkODh1M
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,17 +16,19 @@ public class Shoot : MonoBehaviour
     public float bulletsleft = 35f;
     void Update()
     {
+        //if the player shoots and has bullets left
         if (Input.GetKey(KeyCode.Space) && Time.time >= nextFireTime && bulletsleft > 0)
         {
+            //create a new bullet, set the cooldown, take away from bullets
             Instantiate(bulletPrefab,shootingPoint.position, transform.rotation);
             nextFireTime = Time.time + fireRate;
             bulletsleft--;
         }
-        bulletPrefab.transform.localScale = transform.localScale/20;
+        bulletPrefab.transform.localScale = transform.localScale/20; //scale the bullet so it's the right size
     }
 
      private void OnCollisionEnter2D(Collision2D collision){
-         if(collision.gameObject.tag == "Ammo" && bulletsleft < 35) {
+         if(collision.gameObject.tag == "Ammo" && bulletsleft < 35) { //don't refil full ammo
              bulletsleft += (35 - bulletsleft); //refil ammo to 35;
          }
      }
